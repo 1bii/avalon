08cb08864fa6b5d9a41cd8f784372f3747507bf5
1bii <tan.him.iong@gmail.com>
2019-06-25 00:11:12 -0700

    Updates

---
+++ precache-manifest.fa9129d3aae11205d658ced8ddab7152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ced876753f511d12a7326926e3ace7a",
+    "url": "/avalon/index.html"
+  },
+  {
+    "revision": "2abc439451afc8cd4d3e",
+    "url": "/avalon/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/avalon/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2abc439451afc8cd4d3e",
+    "url": "/avalon/static/js/main.115aa0c6.chunk.js"
+  },
+  {
+    "revision": "3f9c296800172b050d6b",
+    "url": "/avalon/static/js/runtime~main.b7d50eed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avalon/static/media/logo.5d5d9eef.svg"
+  }
+]);
